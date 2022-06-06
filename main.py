0beb9362bfa673ac8513c6e35f3e40b12d768549
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-06 01:30:30 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -8,11 +10,11 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("window-size=1920x1080")
-# chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(
-    executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options
+    service=Service(ChromeDriverManager().install()), options=chrome_options
 )
-
+driver.maximize_window()
 driver.get("https://www.google.com/")
 print(driver.title)
